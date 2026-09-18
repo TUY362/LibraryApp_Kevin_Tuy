ac71e95348dc9bd4973ac1a00827136f4f52f3fe
kevin tuy <ktuy-2026320@kinal.edu.gt>
2026-09-18 08:32:36 -0600

    s1 completa

--- src/org/kt/model/Categoria.java
+++ src/org/kt/model/Categoria.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.kt.model;
 
-/**
- *
- * @author MANI
- */
-public class NewClass {
-    
+public class Categoria {
+    private int idCategoria;
+    private String nombreCategoria;
+
+    public Categoria() {
+    }
+
+    public Categoria(int idCategoria, String nombreCategoria) {
+        this.idCategoria = idCategoria;
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    @Override
+    public String toString() {
+        return nombreCategoria;
+    }
 }
--- src/org/kt/model/LineaVenta.java
+++ src/org/kt/model/LineaVenta.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.kt.model;
 
-/**
- *
- * @author MANI
- */
+//Línea de venta: un libro con su cantidad, usada como fila temporal en la pantalla de venta
+//antes de guardar los DetalleVenta en la base de datos.
 public class LineaVenta {
-    
+    private Libro libro;
+    private int cantidad;
+
+    public LineaVenta() {
+    }
+
+    public LineaVenta(Libro libro, int cantidad) {
+        this.libro = libro;
+        this.cantidad = cantidad;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getIsbn() {
+        return libro.getIsbn();
+    }
+
+    public String getTitulo() {
+        return libro.getTitulo();
+    }
+
+    public double getPrecio() {
+        return libro.getPrecio();
+    }
+
+    public double getSubtotal() {
+        return libro.getPrecio() * cantidad;
+    }
 }
