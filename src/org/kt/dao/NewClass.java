01bd83f2045dff32e246b64e63ab6a985b7c8cce
kevin tuy <ktuy-2026320@kinal.edu.gt>
2026-09-25 06:35:54 +0200

    feat(dao): crear DetalleVentaDAO con Javadoc

---
+++ src/org/kt/dao/NewClass.java
@@ -0,0 +1,14 @@
+package org.kt.dao;
+
+import org.kt.model.DetalleVenta;
+
+/**
+ * Interfaz DAO encargada de definir las operaciones
+ * de acceso a datos para la entidad DetalleVenta.
+ *
+ * Hereda las operaciones CRUD genéricas definidas
+ * en la interfaz Crud.
+ */
+public interface DetalleVentaDAO extends Crud<DetalleVenta, Integer> {
+
+}
